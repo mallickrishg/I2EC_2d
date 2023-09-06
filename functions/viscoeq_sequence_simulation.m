659569dd96bd6def6224973b8657edf14af64c87
Rishav Mallick <mallickrishg@gmail.com>
2023-09-06 09:30:02 -0700

    general framework for N earthquakes in a sequence
    need to test

--- functions/viscoeq_sequence_simulation.m
+++ functions/viscoeq_sequence_simulation.m
@@ -1,4 +1,13 @@
 function [tfull,Yfull] = viscoeq_sequence_simulation(y_odefunction,Y0,rcv,shz,stress_change,Trecur)
+% function to apply initial conditions to a single earthquake sequence ODE (IVP) 
+% and solve the system of equations using Runge-Kutta 4th order integration
+% this code can deal with a single event or a sequence of events
+% INPUTS
+% stress_change - data structure containing 
+%                 Nevents (number of coseismic events)
+%                 Timing (vector of event times - since start of cycle) 
+%                 dtau, dsigma22,disgma23 (matrix of stress changes)
+% Trecur        - earthquake supercycle recurrence time
 
 options=odeset('Refine',1,'AbsTol',1e-10,'RelTol',1e-10,'InitialStep',1e-6,'MaxStep',1e8);
 
@@ -20,29 +29,22 @@ for count = 1:Nevents
         Yfull = Y;
         
         % from event 1 to event 2 OR Trecur
-        Y0 = zeros(rcv.N*rcv.dgf + shz.N*shz.dgf,1);
-        logV_0 = Y(end,1 : rcv.dgf : rcv.dgf*rcv.N)' + stress_change.dtau(:,count)./rcv.Asigma;
-        s22_0 = Y(end,rcv.dgf*rcv.N+1 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N)' + stress_change.dsigma22(:,count);
-        s23_0 = Y(end,rcv.dgf*rcv.N+2 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N)' + stress_change.dsigma23(:,count);
-                
-        Y0(1 : rcv.dgf : rcv.dgf*rcv.N) = logV_0;
-        Y0(rcv.dgf*rcv.N+1 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N) = s22_0;
-        Y0(rcv.dgf*rcv.N+2 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N) = s23_0;
+        Y0 = apply_initialconditions(Y,rcv,shz,stress_change);
 
         [t,Y]=ode45(y_odefunction,[0 Tseqend],Y0,options);
         tfull = [tfull;t + T_events(count)];
         Yfull = [Yfull;Y];
         
     elseif count>1 && count<Nevents
-        Yval = exp(Y(end,:)'); 
-        Y0 = log(rcv.A) + rcv.n.*(log(tau_cs(:,count) + (Yval./rcv.A).^(1./rcv.n)));
+        Y0 = apply_initialconditions(Y,rcv,shz,stress_change);
+        
         [t,Y]=ode45(y_odefunction,[0 T_events(count+1)-T_events(count)],Y0,options);
         tfull = [tfull;t + T_events(count)];
         Yfull = [Yfull;Y];
     
-    else
-        Yval = exp(Y(end,:)'); 
-        Y0 = log(rcv.A) + rcv.n.*(log(tau_cs(:,count) + (Yval./rcv.A).^(1./rcv.n)));
+    else % for last event solve till Trecur (end of cycle)
+        Y0 = apply_initialconditions(Y,rcv,shz,stress_change);
+
         [t,Y]=ode45(y_odefunction,[0 Trecur-T_events(count)],Y0,options);
         tfull = [tfull;t + T_events(count)];
         Yfull = [Yfull;Y];
@@ -50,4 +52,18 @@ for count = 1:Nevents
     end
 end
         
+end
+
+function Y0 = apply_initialconditions(Y,rcv,shz,stress_change)
+% function to apply initial conditions for a given state vector
+
+Y0 = zeros(rcv.N*rcv.dgf + shz.N*shz.dgf,1);
+logV_0 = Y(end,1 : rcv.dgf : rcv.dgf*rcv.N)' + stress_change.dtau(:,count)./rcv.Asigma;
+s22_0 = Y(end,rcv.dgf*rcv.N+1 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N)' + stress_change.dsigma22(:,count);
+s23_0 = Y(end,rcv.dgf*rcv.N+2 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N)' + stress_change.dsigma23(:,count);
+
+Y0(1 : rcv.dgf : rcv.dgf*rcv.N) = logV_0;
+Y0(rcv.dgf*rcv.N+1 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N) = s22_0;
+Y0(rcv.dgf*rcv.N+2 : shz.dgf : rcv.dgf*rcv.N+shz.dgf*shz.N) = s23_0;
+
 end
