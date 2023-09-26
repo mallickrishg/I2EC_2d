65d7707328354d0901a8d5342187ab46784abc50
Rishav Mallick <mallickrishg@gmail.com>
2023-09-25 21:03:08 -0700

    added documentation

--- PLOT_cornerflow_velocityfield.m
+++ PLOT_cornerflow_velocityfield.m
@@ -1,14 +1,29 @@
 clear
+addpath functions/
+
+% specify subduction zone dip angle
 dip = 20*pi/180;
+
+% mesh points to plot
 nx = 1000;
 ny = 500;
 x = linspace(-2,4,nx)';
 z = linspace(-4*tan(dip),-0.001,ny)';
 [X,Z] = meshgrid(x,z);
+
+% compute velocity field in continental & oceanic mantle
+% Vx,Vz are both [Nx2] matrices where
+% Vx(:,1) - continental arc solution
+% Vx(:,2) - oceanic mantle solution
+% similarly for Vz
 [Vx,Vz] = compute_cornerflow_velocityfield(dip,X(:),Z(:));
 
+% Plot velocity field
 figure(1),clf
+
+% solution only applies to continental arc
 index = Z(:)+X(:).*tan(dip)<=0;
+
 nskip = 410;
 toplotx = Vx(:,1);
 toplotz = Vz(:,1);
@@ -17,7 +32,9 @@ toplotz(index) = nan;
 pcolor(x,z,reshape(sqrt(toplotx.^2 + toplotz.^2),ny,nx)), shading interp, hold on
 quiver(X(1:nskip:end)',Z(1:nskip:end)',toplotx(1:nskip:end),toplotz(1:nskip:end),'k-','Linewidth',1)
 
+% solution only applies to oceanic arc
 index = Z(:)+X(:).*tan(dip)>=0;
+
 toplotx = Vx(:,2);
 toplotz = Vz(:,2);
 toplotx(index) = nan;
