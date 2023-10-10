9cae47276505f864367ed93f4eff4a81cad1f747
Rishav Mallick <mallickrishg@gmail.com>
2023-10-10 13:54:49 -0700

    2 or 3 variable outputs depending on traction/stress kernels

--- functions/computeFaultTractionKernelsBem.m
+++ functions/computeFaultTractionKernelsBem.m
@@ -1,4 +1,4 @@
-function [Ktau,Ksigma] = computeFaultTractionKernelsBem(src,rcv,boundary)
+function [varargout] = computeFaultTractionKernelsBem(src,rcv,boundary)
 % traction kernel computation for a given source and receiver object pair
 % INPUTS
 % src,rcv - objects or data structures containing fault mesh 
@@ -6,16 +6,30 @@ function [Ktau,Ksigma] = computeFaultTractionKernelsBem(src,rcv,boundary)
 % boundary - object or data structure containing boundary mesh for BEM
 % 
 % OUTPUTS 
-% 2 x [N x N] matrices containing fault-centric traction kernels
-% these kernels are for half-space only
-% Ktau - traction kernel in shear direction
-% Ksigma - traction kernel in fault-normal direction
+% returns half-space kernels 
+% ONLY resulting from shear slip on a source 
+% based on number of output variables
+% 
+% if varargout == 2 (traction kernels)
+%   2 x [N x N] matrices containing fault-centric traction kernels
+%   Ktau - traction kernel in shear direction
+%   Ksigma - traction kernel in fault-normal direction
+% if varargout == 3 (full stress kernel)
+%   3 x [N x N] matrices containing stress kernels
+%   Kxx,Kzz,Kxz
 % 
 % Author:
 % Rishav Mallick, JPL, 2023
-
-Ktau = zeros(rcv.N,src.N);
-Ksigma = Ktau;
+if nargout == 2
+    Ktau = zeros(rcv.N,src.N);
+    Ksigma = zeros(rcv.N,src.N);
+elseif nargout == 3
+    Kxx = zeros(rcv.N,src.N);
+    Kzz = zeros(rcv.N,src.N);
+    Kxz = zeros(rcv.N,src.N);
+else
+    error('incorrect number of output options. provide either 2 (traction) or 3 (stress) output variables.')
+end
 
 % compute kernels relating unit shear slip from src to boundary
 [Kdd_src_boundary,Kdn_src_boundary,~,~] = geometry.computeFullTractionKernels(src,boundary);
@@ -28,10 +42,18 @@ Ktraction = [Kdd,Knd;Kdn,Knn];
 Kdisp = [Gdx,Gnx;Gdz,Gnz];
 
 % src to rcv kernels
-[Kdd_src_rcv,Kdn_src_rcv,~,~] = geometry.computeFullTractionKernels(src,rcv);
+% and boundary to rcv kernels
+if nargout == 2
+    [Kdd_src_rcv,Kdn_src_rcv,~,~] = geometry.computeFullTractionKernels(src,rcv);
+    [Kdd_boundary_rcv,Kdn_boundary_rcv,Knd_boundary_rcv,Knn_boundary_rcv] = ...
+        geometry.computeFullTractionKernels(boundary,rcv);
+elseif nargout == 3
+    [Kdxx_src_rcv,Kdzz_src_rcv,Kdxz_src_rcv,~,~,~] = geometry.computeFullStressKernels(src,rcv);
+    [Kdxx_boundary_rcv,Kdzz_boundary_rcv,Kdxz_boundary_rcv,Knxx_boundary_rcv,Knzz_boundary_rcv,Knxz_boundary_rcv] = geometry.computeFullStressKernels(boundary,rcv);
+else
+end
 
 % boundary to rcv kernels
-[Kdd_boundary_rcv,Kdn_boundary_rcv,Knd_boundary_rcv,Knn_boundary_rcv] = geometry.computeFullTractionKernels(boundary,rcv);
 
 for i = 1:src.N
     % we only consider shear sources
@@ -66,14 +88,33 @@ for i = 1:src.N
     % BEM solve
     bem_sol = kernel\BC;
     
-    % compute tractions from bem solution
-    tau = [Kdd_src_rcv;Kdn_src_rcv]*source + ...
-        [Kdd_boundary_rcv,Knd_boundary_rcv;Kdn_boundary_rcv,Knn_boundary_rcv]*bem_sol;
+    % compute tractions or stress from bem solution
     
-    Ktau(:,i) = tau(1:end/2);
-    Ksigma(:,i) = tau(end/2+1:end);
+    if nargout == 2
+        tau = [Kdd_src_rcv;Kdn_src_rcv]*source + ...
+            [Kdd_boundary_rcv,Knd_boundary_rcv;Kdn_boundary_rcv,Knn_boundary_rcv]*bem_sol;
+
+        Ktau(:,i) = tau(1:end/2);
+        Ksigma(:,i) = tau(end/2+1:end);        
+    elseif nargout == 3
+        sigma = [Kdxx_src_rcv;Kdzz_src_rcv;Kdxz_src_rcv]*source + ...
+            [Kdxx_boundary_rcv,Knxx_boundary_rcv;Kdzz_boundary_rcv,Knzz_boundary_rcv;Kdxz_boundary_rcv,Knxz_boundary_rcv]*bem_sol;
+        Kxx(:,i) = sigma(1:end/3);
+        Kzz(:,i) = sigma(end/3+1:2/3*end);
+        Kxz(:,i) = sigma(2/3*end+1:end);
+    else
+
+    end
 end
 
-
+if nargout == 2
+    varargout{1} = Ktau;
+    varargout{2} = Ksigma;
+elseif nargout == 3
+    varargout{1} = Kxx;
+    varargout{2} = Kzz;
+    varargout{3} = Kxz;
+else
+end
 
 end
